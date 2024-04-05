2e09919559d2f19eda58c23c15144a70bc28d61f
akashsharma99 <akash.asharma.sharma11@gmail.com>
2024-04-05 11:55:50 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/akashsharma99/encdec
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+// cli tool to take a string and encode it to base64
+func main() {
+	fmt.Println("Hello, World!")
+}
